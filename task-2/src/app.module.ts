51cbd5092e134d104661b2a5b69ca1a0454d1e55
Роман <tarkauf@gmail.com>
2026-04-09 11:37:15 +0300

    1

--- task-2/src/app.module.ts
+++ task-2/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { PassengersModule } from './passengers/passengers.module';
 import { AirlinesModule } from './airlines/airlines.module';
 import { DatasourceModule } from './datasource/datasource.module';
-import { SouvenirsModule } from './countries/countries.module';
+import { CountriesModule } from './countries/countries.module';
 @Module({
-  imports: [PassengersModule, AirlinesModule , SouvenirsModule, DatasourceModule],
+  imports: [PassengersModule, AirlinesModule , CountriesModule, DatasourceModule],
   controllers: [],
   providers: [],
 })
